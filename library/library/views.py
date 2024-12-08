9fa72bd3eb2b84b554ec51f58be983c1e2a7228f
Dmytro Potapchuk <traiget@gmail.com>
2024-12-08 11:26:08 +0200

    first commit

---
+++ library/library/views.py
@@ -0,0 +1,10 @@
+# from django.http import HttpResponse
+from django.shortcuts import render
+
+def homepage(request):
+    # return HttpResponse("Hello world! It's main page")
+    return render(request, 'home.html')
+
+def about(request):
+    # return HttpResponse("My about page.")
+    return render(request, 'about.html')
