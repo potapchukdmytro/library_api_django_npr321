31cacef9176fdead5be2989633aa1a832ccb9ac0
potapchukdmytro <traiget@gmail.com>
2025-01-12 10:56:25 +0200

    register

--- library/library/urls.py
+++ library/library/urls.py
@@ -24,7 +24,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.homepage),
     path('about/', views.about),
-    path('books/', include('books.urls'))
+    path('books/', include('books.urls')),
+    path('users/', include('users.urls'))
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
